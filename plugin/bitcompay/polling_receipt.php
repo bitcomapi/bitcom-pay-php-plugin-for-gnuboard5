98ff6b702edf7b41df809a85e88b7e870b17f424
toby <hoeseung18k@gmail.com>
2019-02-22 11:30:51 +0900

    SQL fix

--- plugin/bitcompay/polling_receipt.php
+++ plugin/bitcompay/polling_receipt.php
@@ -2,7 +2,7 @@
 include_once('./_common.php');
 
 if (1 == is_numeric($_REQUEST['ORN'])) {
-  $sql = " SELECT po_rel_action AS RECEIPT FROM g5_point WHERE po_rel_action IN ('BitcomPay-ORN-{$nOrderNum}') ";
+  $sql = " SELECT po_rel_action AS RECEIPT FROM g5_point WHERE po_rel_action IN ('BitcomPay-ORN-{$_REQUEST['ORN']}') ";
   $result = sql_query($sql);
   $row = sql_fetch_array($result);
   
